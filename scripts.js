f5358d1d4bf81aefdf618d31dd158c6c77ad1ace
z_yoosefi <z_yoosefi@rasana.ir>
2018-09-25 18:24:49 +0330

    use localStorage

--- scripts.js
+++ scripts.js
@@ -1,51 +1,90 @@
-function AddToList(){
-    var input = document.getElementById("inputText");
-    var inputValue = input.value;
-    var ULContent =  document.getElementById("ULContent");
-    contentAlert = document.getElementById("content-alert");
-    if(inputValue === '') {
-        contentAlert.innerHTML="please add your new todo";
-    }
-    else {
-        render(input,inputValue,ULContent);
-    }
-    input.focus();
+var myList = JSON.parse(localStorage.getItem('myList') || '[]');
+var ULContent =  document.getElementById("ULContent");
+var input = document.getElementById("inputText");
+
+function clearULContainer() {
+    ULContent.innerHTML = " ";
 }
 
-//add item
-function render (input, inputValue, ULContent) {
-    contentAlert.innerHTML = '';
-    input.value = '';
+function removeItemFromList() {
+    var deleteItems = document.getElementsByClassName("delete");
+    var i;
+    for(i = 0; i < deleteItems.length ; i++){
+        (function(i) {
+            deleteItems[i].onclick = function(){
+                var li =  this.parentElement;
+                li.className = "hidden";
+                // var x = myList.splice(myList.indexOf(li.firstChild.nodeValue), 1);
+                var x = myList.splice(i, 1);
+                console.log(li.firstChild.nodeValue);
+                updateLocalStorage();        
+            }
+        })(i)
+    }
+}
+
+function doUndo(li) {
+    li.isDone = !li.isDone;
+    updateLocalStorage();
+    render(); 
+}
+
+function createLiElement(li) {
     var newLi = document.createElement("li");
+    newLi.appendChild(document.createTextNode(li.value));
     ULContent.appendChild(newLi);
-    newLi.appendChild(document.createTextNode(inputValue));
     var span = document.createElement("span");
     span.appendChild(document.createTextNode("delete"));
     span.className = "delete";
     newLi.appendChild(span);
-
+    if (li.isDone) {
+        newLi.style.textDecoration = 'line-through';
+    }
+    
     //remove item
-    removeFromList();
+    removeItemFromList();
+    newLi.onclick = function () { doUndo(li); };
+}
+function updateLocalStorage() {
+    localStorage.setItem('myList', JSON.stringify(myList));
 }
 
-//remove item
-function removeFromList() {
-    var deleteItems = document.getElementsByClassName("delete");
-    var i;
-    for(i = 0; i < deleteItems.length ; i++){
-        deleteItems[i].onclick = function(){
-        var li =  this.parentElement;
-        li.className = "hidden";
-        }
+function render () {
+    clearULContainer() ;
+    for( var i = 0; i < myList.length ; i++) {
+         createLiElement(myList[i]);
     }
 }
 
-//doUndo item
-var ul = document.querySelector("ul");
-ul.addEventListener('click' , function(ev) {
-   if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+function AddToArray(inputValue) {
+    myList.push({ value: inputValue});
+    updateLocalStorage();
+    render();
 }
-});
+
+function Add(){
+    var inputValue = input.value;
+    if (inputValue) {
+        AddToArray(inputValue);
+    }
+    input.focus();
+    input.value = '';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
